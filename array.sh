e9f8aff69174a9907bc5dee1ef503457a0be4471
Leandro Aurelio <leandro.aurelio@hotmail.com>
2021-09-28 15:24:04 -0300

    boson01

---
+++ array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Array cria multiplas variaveis, exemplo
+testearray=(laranja morango acerola abacaxi)
+
+#Para ver a array, é possível com o comando echo, exemplo:
+echo ${testearray[1]}
+
+#Para mostrar todos
+echo ${testearray[*]}
